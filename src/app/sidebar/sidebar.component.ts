bd6c7115fb2b6c4c756ec7d6f5322aaf13e03831
fauziahmay <fauziah222102534@gmail.com>
2024-12-21 21:00:11 +0700

    apoo

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  @Input() moduleName: string = '';
+  username: string = '';
 
-  @Input() moduleName: string = "";
-  
+  constructor(private cookieService: CookieService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.username = this.cookieService.get('userId');
+  }
 }
